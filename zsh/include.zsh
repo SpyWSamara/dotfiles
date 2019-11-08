bca24877a26763ee5e06ac8350fb13eb1aff3f63
SpyWSamara <spywsamara@gmail.com>
2019-11-08 21:09:21 +0400

    Fix: exit code not zero
    If file `/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh`
    not exists.

--- zsh/include.zsh
+++ zsh/include.zsh
@@ -4,4 +4,6 @@ source $DOTFILES_ZSH/env.zsh
 source $DOTFILES_ZSH/aliases.zsh
 source $DOTFILES_ZSH/functions.zsh
 
-[ -e /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [[ -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] then
+    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
