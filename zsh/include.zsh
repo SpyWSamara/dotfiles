9b98b1c85e48276e84fda52d40f4e6a6fe49106f
SpyWSamara <spywsamara@gmail.com>
2019-11-08 11:00:03 +0400

    Add `zsh-syntax-highlighting`
    Install `sudo dnf install zsh-syntax-highlighting`

--- zsh/include.zsh
+++ zsh/include.zsh
@@ -3,3 +3,6 @@ export DOTFILES_ZSH=${0:a:h}
 source $DOTFILES_ZSH/env.zsh
 source $DOTFILES_ZSH/aliases.zsh
 source $DOTFILES_ZSH/functions.zsh
+
+
+[ -e /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
